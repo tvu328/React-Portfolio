6a3b3c78189707b6d2cb8cebcca327cba426e533
Tyler <tylerlvvu@gmail.com>
2023-03-16 16:23:13 -0700

    adding project 3 to portfolio

---
+++ pages/portfolio.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Portfolio() {
+    return (
+        <div className= 'mainBody'>
+            <h1 className='title'>Portfolio</h1>
+            <Project title="pics-R-us" details="Users can share, post, and interact with their favorite topics displayed in galleries." link='https://picrus.netlify.app/?gallery=technology'/>
+        </div>
+    )
+}
